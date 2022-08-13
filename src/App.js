4bcbd0450628081c9d1fc18c70686fb02689578c
Bryan Herrera <bcherrera14@gmail.com>
2022-08-12 20:37:02 -0700

    create stats component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import ProductivityForm from './components/ProductivityForm';
+import Stats from './components/Stats';
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<div className="container d-flex justify-content-center">
+			<div className="container d-flex flex-column align-items-center">
 				<ProductivityForm />
+				<Stats />
 			</div>
 		</div>
 	);
---
+++ src/components/Stats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Stats() {
+	return (
+		<div className="d-flex justify-content-between text-white w-75 mt-3">
+			<p>Average Productivity : 8</p>
+			<p>Averge Hours of Sleep : 8.5</p>
+		</div>
+	);
+}
+
+export default Stats;
